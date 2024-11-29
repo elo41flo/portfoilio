f0b02159d1e5335946bad9a3b27ba32d55a47dd4
eloflo41 <elo.robert41@gmail.com>
2024-11-29 13:00:37 +0100

    mise à jour tarifs

---
+++ index.js
@@ -0,0 +1,4 @@
+// Sélectionne le lien et ajoute un événement de clic
+document.querySelector('.btn-download').addEventListener('click', function() {
+    alert('Le téléchargement du PDF a commencé!');
+});
